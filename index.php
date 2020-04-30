0b13cba2ae8bbcbfd940416a0cddc581f4ea0e56
Ranneciva <63185932+Ranneciva@users.noreply.github.com>
2020-05-01 05:28:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
   <h1>Uji coba untuk belajar GITHUB</h1>
 <p> Github merupakan salahsatu layanan cloud yang dapat dihubungkan ke GIT untuk melakukan Version Control System yang terintegrasi.</p>
   
-  <p> Selanjutnya, kita melakukan percobaan commit baru pada repo test-dulu di index.php </p>
-  
+  <p> Menghapus line 12 dan menambahkan line baru pada line 12 </p>
 </body>
 
 </html>
