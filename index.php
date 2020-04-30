42c9124c825f54c80363f37321509bfd8f1d78c1
Ranneciva <63185932+Ranneciva@users.noreply.github.com>
2020-05-01 05:26:21 +0700

    Membuat index html
    Untuk testing index htmlnya

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Uji Coba</title>
+</head>
+
+<body>
+  <h1>Uji coba untuk belajar GITHUB</h1>
+<p> Github merupakan salahsatu layanan cloud yang dapat dihubungkan ke GIT untuk melakukan Version Control System yang terintegrasi.</p>
+</body>
+
+</html>
